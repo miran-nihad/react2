be223028e4aea7a66c205b9f5a7f26197e1081b8
miran-nihad <miran-walashy@hotmail.com>
2024-02-15 10:25:07 +0100

    test

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './components/Home'
+import Categories from './components/Categories'
+import Layout from './components/Layout'
+import Category from './components/category'
+import CategoriesIndex from './components/CategoriesIndex'
+
+function App() {
+  
+
+  return (
+    <Layout>
+      <Routes>
+        <Route index element={<Home/>} />
+        <Route path="categories/*" element={<Categories/>} >
+          <Route index element={<CategoriesIndex />}/>
+          <Route path=':slug' element={<Category />}/>
+        </Route>
+    </Routes>
+    </Layout>
+  )
+}
+
+export default App
---
+++ src/components/Layout.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+export default function Layout({children}) {
+    return(
+        <>
+        <header>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="categories">Categories</Link></li>
+                </ul>
+            </nav>
+        </header>
+        {children}
+        <footer>Footer</footer>
+        </>
+    )
+}
